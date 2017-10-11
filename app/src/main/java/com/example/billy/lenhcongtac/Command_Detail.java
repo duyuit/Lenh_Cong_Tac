e9fa8684ceeb9aeadb5479f21c6e61c63df4cf39
duyuit <duykkxm92@gmail.com>
2017-10-11 21:35:18 +0700

    1

---
+++ app/src/main/java/com/example/billy/lenhcongtac/Command_Detail.java
@@ -0,0 +1,13 @@
+package com.example.billy.lenhcongtac;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Command_Detail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_command__detail);
+    }
+}
